6a9c2f43c9ef166048bd376b563154997c247fd5
Azat IOS <ios.azatech@Azat-MacBook-Air.local>
2017-08-08 13:18:13 +0300

    Need to add new UI

--- ITorch/ViewController.swift
+++ ITorch/ViewController.swift
@@ -7,19 +7,92 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+
+
+    @IBOutlet weak var btnImage: UIButton!
+//    var flag = 0
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+//        let image1 = UIImage(named: "lampOn.png") as UIImage!
+//        self.btnImage.setImage(image1, for: .normal)
+        func btn_Pressed(_ sender: UIButton) {
+
+        let device = AVCaptureDevice.defaultDevice(withMediaType: AVMediaTypeVideo)
+
+            if (device?.hasTorch)! {
+            do {
+                try device?.lockForConfiguration()
+
+                let image1 = UIImage(named: "lampOff2.png") as UIImage!
+                btnImage.setImage(image1, for: .selected)
+
+                let image2 = UIImage(named: "kubik.png") as UIImage!
+                btnImage.setImage(image2, for: .selected)
+//                print("3sdvhsui3")
+//                print("4sdvpos4")
+
+                device?.torchMode = device?.torchMode == AVCaptureTorchMode.on ? .off : .on
+                device?.unlockForConfiguration()
+
+                func name1() {
+                    print("11")
+                }
+
+
+                func name2() {
+                    print("22")
+                }
+
+            } catch {
+                print(error)
+            }
+        }
     }
+}
+
+
+
+//
+////                        let playButton  = UIImage(named: "lampOn.png") as UIImage!
+//
+//                        if AVCaptureTorchMode.on == AVCaptureTorchMode.on {
+//                          print("11")
+//                            let image1 = UIImage(named: "lampOn.png") as UIImage!
+//                            self.btnImage.setImage(image1, for: .normal)
+//
+//         //                   playButton.setImage(UIImage(named: "lampOn.png"), for: .normal)
+//
+//                        } else if AVCaptureTorchMode.off == AVCaptureTorchMode.off {
+////                            playButton.setImage(UIImage(named: "lampOff2.png"), for: .normal)
+//                            print("22")
+//                            let image2 = UIImage(named: "lampOff2.png") as UIImage!
+//                            self.btnImage.setImage(image2, for: .normal)
+//                        }
+//                        device?.unlockForConfiguration()
+//        
+//                    } catch {
+//                        print(error)
+//                    }
+//            }
+//        }
+//    }
+
+
+//    @IBAction func actionTorch(_ sender: UIButton) {
+//        let device = AVCaptureDevice.defaultDevice(withMediaType: AVMediaTypeVideo)
+//
+//        let playButton  = UIButton(type: .custom)
+//
+//
+//        }
+
+
 
 
 }
-
